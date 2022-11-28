288f99302af7df4a1170bbed4b37f0dc20ab9c12
YoungCheon Kim <kyt031522@gmail.com>
2022-11-29 03:24:28 +0900

    gitignore

--- index.js
+++ index.js
@@ -1,18 +1,45 @@
 const express = require('express')
 const app = express()
 const port = 3000
+const bodyParser = require('body-parser');
 
-const mongoose = require('mongoose')
-mongoose.connect('mongodb+srv://kych0912:7uWJAnFu62qp6g54@cluster0.uchta0w.mongodb.net/?retryWrites=true&w=majority',{
+//application/x-www-form-urlencoded 정보를 분석
+app.use(bodyParser.urlencoded({extended:true}));
+//application/json 정보 분석
+app.use(bodyParser.json());
+
+const config = require('./config/key');
+
+const {User} = require("./models/User");
+
+const mongoose = require('mongoose');
+
+mongoose.connect(config.mongoURI,{
     useNewUrlParser:true,useUnifiedTopology:true
     }).then(()=> console.log("MongoDB Connected..."))
-    .catch(err=>console.log(err))
+    .catch(err=>console.log(err));
+
+
+
+
 
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
-})
+});
+
+app.post('/register',(req,res) =>{
+  //app은 express, user는 mongoose model
+  const user = new User(req.body);
+  //mongoose method
+  user.save((err,userInfo)=>{
+    if(err) return res.json({success:false,err})
+    return res.status(200).json({
+      success:true
+    })
+  })
+});
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
-})
+});
